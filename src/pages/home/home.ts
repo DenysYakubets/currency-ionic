3becdd122b6cc658a7d85c61c61017b778d68b6c
DenysYakubets <w.denisyakubets@gmail.com>
2017-06-03 02:47:19 +0300

    Showing list of currency name and rate

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
+import { Observable } from 'rxjs/Observable';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ApiService } from '../../shared/api.service';
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [ApiService]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: any;
 
+  constructor(public navCtrl: NavController, private api: ApiService) {}
+
+  ionViewDidLoad(){
+    this.api.getData("USD").subscribe(
+      result => this.items = result
+    );
+  }
+
+  onChangeSeacrhBar(event, value){
+    console.log(value);
   }
 
 }
